3acdddbe2183ddf2f7226d1cef4ebc20f9e4b2d4
Sidachenko Michael <michaellux@yandex.ua>
2024-08-26 14:10:57 +0300

    add sort logic

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import ThingItem from '@/components/ThingItem.vue';
 import { ref } from 'vue';
 import userThingsData from '@data/user-things.json';
 import chooseThingsData from '@data/choose-things.json';
-
 const userThings = ref(userThingsData);
 const chooseThings = ref(chooseThingsData);
 
 const selectedUserThings = ref([]);
+const selectedUserThingsOrderId = ref([]);
 const chooseThing = ref({});
 
 const addToSelectedUserThings = (id) => {
@@ -18,6 +18,19 @@ const addToSelectedUserThings = (id) => {
   (selectedUserThings.value.length === 6 && targetItem.isSelected === true)) {
     targetItem.isSelected = !targetItem.isSelected;
     selectedUserThings.value = userThings.value.filter((item) => item.isSelected === true);
+
+    selectedUserThings.value.sort((a, b) => 
+      selectedUserThingsOrderId.value.indexOf(a.id) - selectedUserThingsOrderId.value.indexOf(b.id)
+    );
+
+    if (targetItem.isSelected) {
+      const existingIndex = selectedUserThings.value.findIndex(item => item.id === id);
+      selectedUserThings.value.splice(existingIndex, 1);
+      selectedUserThings.value.push(targetItem);
+      selectedUserThingsOrderId.value = selectedUserThings.value.map((item) =>item.id);
+    } else {
+      selectedUserThingsOrderId.value = selectedUserThingsOrderId.value.filter((currentId) => currentId !== id);
+    }
   }
 }
 
